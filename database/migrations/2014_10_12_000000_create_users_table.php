3de510590a565d952a0513a45b5a60fc94ce1345
hinako-m <muratani@lassic.co.jp>
2020-05-20 01:54:25 +0000

    user registration add fontawsome

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('ユーザーID');
+            $table->string('Email')->unique();
+            $table->string('パスワード');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark"> 
+        <a class="navbar-brand" href="/">レシピ紹介サイトらくうまレシピ</a>
+         
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <i class="fas fa-camera"></i>
+                <button type="button" class="btn btn-primary"><i class="fas fa-camera"></i><span class="glyphicon glyphicon-search"></span> 検索</button>
+            </ul>
+        </div>
+    </nav>
+</header>
